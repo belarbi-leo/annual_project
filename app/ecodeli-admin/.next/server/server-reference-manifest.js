be9df48c555cf480e70c459a8d4f10006fe7cd48
Enzo Afonso <afonsoesgi@proton.me>
2025-03-29 14:31:59 +0100

    fix: deleted .next folders from git following

--- app/ecodeli-admin/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FjJxkd/yMCDKaPsEwxb82MzppWC1X8peSgl0QFLNmMc=\"\n}"
--- app/ecodeli-public/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"u6kbjZNVquZ5lZhLoPrwnhu/aC2534AY027wEpN2O34=\"\n}"
